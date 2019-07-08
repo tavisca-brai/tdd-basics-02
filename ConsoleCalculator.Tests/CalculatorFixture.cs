ae57d8f3ea7bfd6a89d417c5f28d0ecb52284bd2
tavisca-brai <brai@tavisca.in>
2019-07-08 10:53:54 +0530

    tdd calculator kata

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -1,14 +1,73 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+
         [Fact]
-        public void DummyTest()
+        public void Test1()
         {
-            return;
+            string exp = "0";
+            Assert.Equal("0", ActualString(exp));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            string exp = "000";
+            Assert.Equal("0", ActualString(exp));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            string exp = "1/0=";
+            Assert.Equal("-E-", ActualString(exp));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            string exp = "3+4";
+            Assert.Equal("4", ActualString(exp));
+        }
+
+
+        [Fact]
+        public void Test7()
+        {
+            string exp = "3..5+4..5=";
+            Assert.Equal("8", ActualString(exp));
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            string exp = "3..5+4..5+2+1..5+0.5=";
+            Assert.Equal("12", ActualString(exp));
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            string exp = "3..5+4.c";
+            Assert.Equal("", ActualString(exp));
+        }
+
+
+
+        private string ActualString(string exp)
+        {
+            Calculator calculator = new Calculator();
+            string result = string.Empty;
+            foreach (var item in exp)
+            {
+                result = calculator.SendKeyPress(item);
+            }
+            return result;
         }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -4,11 +4,125 @@ namespace ConsoleCalculator
 {
     public class Calculator
     {
+        static string operand = string.Empty;
+        static string temp = string.Empty;
+        static string op = string.Empty;
 
         public string SendKeyPress(char key)
         {
             // Add your implementation here.
-            throw new NotImplementedException();
+            if (char.IsLetter(key))
+                key = char.Parse(key.ToString().ToLower());
+            switch (key)
+            {
+                case var v when (char.IsDigit(key) || key == '.' || key == 's'):
+                    return GetDigit(key);
+                case var v when (key == '+' || key == '-' || key == 'x' || key == '/'):
+                    RecievedOperator(key);
+                    break;
+                case 'c':
+                    Reset();
+                    break;
+                case '=':
+                    if (!string.IsNullOrEmpty(op))
+                    {
+                        string s = op;
+                        op = string.Empty;
+                        return Result(s);
+                    }
+                    break;
+                default:
+                    return "";
+
+            }
+
+            return operand;
+        }
+
+        private string Result(string op)
+        {
+            double answer = 0;
+            switch (op)
+            {
+                case "+": answer = double.Parse(operand) + double.Parse(temp); break;
+                case "-": answer = double.Parse(operand) - double.Parse(temp); break;
+                case "/":
+                    if (temp == "0")
+                        return "-E-";
+                    else
+                        answer = double.Parse(operand) / double.Parse(temp); break;
+                case "x": answer = double.Parse(operand) * double.Parse(temp); break;
+            }
+            temp = string.Empty;
+
+            return operand = answer.ToString();
+        }
+
+        private void Reset()
+        {
+            operand = string.Empty;
+            temp = string.Empty;
+            op = string.Empty;
+        }
+
+        private void RecievedOperator(char key)
+        {
+
+            if (string.IsNullOrEmpty(op) && !string.IsNullOrEmpty(operand))
+            {
+                op = key.ToString();
+            }
+            else
+            {
+                Result(op);
+                op = key.ToString();
+            }
+        }
+
+        private string GetDigit(char key)
+        {
+            if (string.IsNullOrEmpty(op))
+            {
+                if (key == 's')
+                    return operand = (double.Parse(operand) * (-1)).ToString();
+                if (key == '.')
+                {
+                    if (!operand.Contains("."))
+                        return operand += ".";
+                    else
+                        return operand;
+                }
+
+                if (!(operand == "0" && key == '0'))
+                {
+                    if (operand == "0")
+                        operand = key.ToString();
+                    else
+                        operand += key.ToString();
+                }
+                return operand;
+            }
+            else
+            {
+                if (key == 's')
+                    return temp = (double.Parse(temp) * (-1)).ToString();
+                if (key == '.')
+                {
+                    if (!temp.Contains("."))
+                        return temp += ".";
+                    else
+                        return temp;
+                }
+
+                if (!(temp == "0" && key == '0'))
+                {
+                    if (temp == "0")
+                        temp = key.ToString();
+                    else
+                        temp += key.ToString();
+                }
+                return temp;
+            }
         }
     }
 }
